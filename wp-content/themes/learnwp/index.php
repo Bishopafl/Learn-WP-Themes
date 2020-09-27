792d11daca29bea18010bdee36708f4e75728529
Adam Lopez <adamf04@gmail.com>
2020-09-27 18:16:50 -0400

    code reform, using get_template_part function to show the content of a post from another folder

--- wp-content/themes/learnwp/index.php
+++ wp-content/themes/learnwp/index.php
@@ -29,18 +29,8 @@
                                     // While have posts, show them to us please
                                     while ( have_posts() ): the_post();
 
-                            ?>
-                                    <!-- loop through and display posts using wp functions -->
-                                    <article>
-                                        <h2><?php the_title(); ?></h2>
-                                        <?php the_post_thumbnail( array(275,275) ); ?>
-                                        <p>Posted in <?php echo get_the_date(); //when getting something from wp, gotta echo ?> by <?php the_author_link(); ?></p>
-                                        <p>Categories <?php the_category( ' ' ); ?></p>
-                                        <p><?php the_tags('Tags:', ', '); ?></p>
-                                        <p><?php the_content(); ?></p>
-                                    </article>
+                                    get_template_part( 'template-parts/content' );
 
-                            <?php 
                                     endwhile;
                                     else:
                             ?>
---
+++ wp-content/themes/learnwp/template-parts/content.php
@@ -0,0 +1,9 @@
+<!-- loop through and display posts using wp functions -->
+<article>
+    <h2><?php the_title(); ?></h2>
+    <?php the_post_thumbnail( array(275,275) ); ?>
+    <p>Posted in <?php echo get_the_date(); //when getting something from wp, gotta echo ?> by <?php the_author_link(); ?></p>
+    <p>Categories <?php the_category( ' ' ); ?></p>
+    <p><?php the_tags('Tags:', ', '); ?></p>
+    <p><?php the_content(); ?></p>
+</article>
