541b795952343b85d2120da34c34bbc3e4585992
Adam Lopez <adlopez@masonite.com>
2020-10-04 16:32:16 -0400

    additional code re-formatting. adding styles for the overall look and apearance. WIll definitely change the styles later though as I do not like how they initlally look. Overall concept is there for blog posts, css is also styled differently for each formatted post type. pretty cool.

--- wp-content/themes/learnwp/functions.php
+++ wp-content/themes/learnwp/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function load_scripts() {
-    wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
     wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js', array('jquery'), '3.4.1', true );
     wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css', array(), '3.4.1', 'all' );
+    wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
 
@@ -15,14 +15,12 @@ function learnwp_config() {
             'footer_menu' => 'Footer Menu'
         )
     );
-
     // custom header themes
     $args = array(
         'height'    => 225,
         'width'     => 1920
     );
     add_theme_support( 'custom-header', $args);
-
     // add thumbnail support
     add_theme_support( 'post-thumbnails' );
     // add formatting menu to theme
