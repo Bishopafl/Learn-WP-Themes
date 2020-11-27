009c605f5c1f6afa93a81224762e70a1a11bbdd0
Adam Lopez <adamf04@gmail.com>
2020-11-27 12:24:11 -0500

    Added a search form at the top of the navigation bar, also added a search box within the search page that displays what the query string that was searched for. Did some style updates as well, removed the button within the search form

--- wp-content/themes/learnwp/search.php
+++ wp-content/themes/learnwp/search.php
@@ -4,8 +4,12 @@
     <div id="main">
         <div class="container">
 
+            <h2>Search results for:<?php echo get_search_query(); ?></h2>
+
             <?php 
-            
+                
+                get_search_form(); 
+
                 while ( have_posts() ) :
                     the_post();
                     get_template_part('template-parts/content', 'search');
