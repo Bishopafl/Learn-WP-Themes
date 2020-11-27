417da026acb96ceb5e1fb22a0dc5e6b38dfde9f5
Adam Lopez <adamf04@gmail.com>
2020-11-27 14:55:43 -0500

    adding pagination links on single posts and within the search page. Also changing some of the template styles for the theme

--- wp-content/themes/learnwp/search.php
+++ wp-content/themes/learnwp/search.php
@@ -18,6 +18,13 @@
                         comments_template();
                     endif;
                 endwhile;
+
+                the_posts_pagination(
+                    array(
+                        'prev_text' => 'Previous',
+                        'next_text' => 'Next'
+                    )
+                );
             
             ?>
 
